897e5018821aee7231e49f4a47e9cd9bd55d9119
BoYuanVisionary <yuanbo096@gmail.com>
2025-01-22 19:08:05 +0000

    install

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -1,4 +1,4 @@
 # These are APT updates needed for use on Runpod
 apt update -y && apt upgrade -y && apt install -y nano &&
 pip install jupyterlab ipywidgets jupyterlab-widgets --upgrade &&\
-sh install_packages.sh
+sh /workspace/projects/interpretable-moes/install_packages.sh
