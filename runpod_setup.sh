3ed451a7aa72d2747ab37ae7c4f48d0698586f7e
BoYuanVisionary <yuanbo096@gmail.com>
2025-01-23 01:43:31 +0000

    Revert "install"
    This reverts commit 897e5018821aee7231e49f4a47e9cd9bd55d9119.

--- runpod_setup.sh
+++ runpod_setup.sh
@@ -1,4 +1,4 @@
 # These are APT updates needed for use on Runpod
 apt update -y && apt upgrade -y && apt install -y nano &&
 pip install jupyterlab ipywidgets jupyterlab-widgets --upgrade &&\
-sh /workspace/projects/interpretable-moes/install_packages.sh
+sh install_packages.sh
